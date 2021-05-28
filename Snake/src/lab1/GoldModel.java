629442e992f0c44481e96f9f84c8283681485fa5
Adrián Litardo <lalitard@espol.edu.ec>
2021-05-27 19:05:19 -0500

    Modificando color moneda y serpiente, tambien botom start

--- Snake/src/lab1/GoldModel.java
+++ Snake/src/lab1/GoldModel.java
@@ -57,7 +57,7 @@ public class GoldModel extends GameModel {
 	/** Graphical representation of a coin. */
 	private static final GameTile COIN_TILE = new RoundTile(new Color(255, 215,
 			0),
-			new Color(255, 255, 0), 2.0);
+			new Color( 0, 0, 255), 2.0);
 
 	
 			
